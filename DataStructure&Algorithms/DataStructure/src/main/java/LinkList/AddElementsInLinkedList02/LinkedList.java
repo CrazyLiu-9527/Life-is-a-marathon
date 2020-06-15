7417a2b958ae46b4b49a43f27892aa4e938240d3
liuzhiyuan <369929712@qq.com>
2020-06-15 23:41:36 +0800

    Add datastructure linkedlist

---
+++ DataStructure&Algorithms/DataStructure/src/main/java/LinkList/AddElementsInLinkedList02/LinkedList.java
@@ -0,0 +1,90 @@
+package LinkList.AddElementsInLinkedList02;
+
+/**
+ * 在链表中添加元素
+ *
+ * @author liuzy
+ * @date 2020/6/15 23:02
+ */
+public class LinkedList<E> {
+
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = new Node();
+        size = 0;
+    }
+
+    //获取链表中的个数
+    public int getSize() {
+        return size;
+    }
+
+    //返回链表是否为空
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    //在链表头添加新的元素
+    public void addFirst(E e) {
+//        Node node = new Node(e);
+//        node.next = head;
+//        head = node;
+
+        head = new Node(e, head);
+        size++;
+    }
+
+    //在链表的index位置添加新的元素
+    //在链表中不是一个常用的操作，练习用
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Illegal index.");
+        }
+
+        if (index == 0) {
+            addFirst(e);
+        } else {
+            Node prev = head;
+            for (int i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+
+//            Node node = new Node(e);
+//            node.next = prev.next;
+//            prev.next = node;
+
+            prev.next = new Node(e, prev.next);
+            size++;
+        }
+    }
+
+    //在链表的末尾添加元素
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+}
