e77c1e6d07a4683a3001751305aec11405fc299e
zhiyuanliu <zhiyuanliu@ctrip.com>
2020-06-24 17:50:01 +0800

    Add files

--- DataStructure&Algorithms/Algorithms/src/main/java/SortingAlgorithm/BubbleSort.java
+++ DataStructure&Algorithms/Algorithms/src/main/java/SortingAlgorithm/BubbleSort.java
@@ -2,6 +2,7 @@ package SortingAlgorithm;
 
 /**
  * 冒泡排序算法
+ * 时间复杂度O（n^2）
  * @author zhiyuanliu
  * @date 2020/6/24 14:40
  */
@@ -20,13 +21,72 @@ public class BubbleSort {
      * 如果一趟排序后也没有交换位置，那么该数组已有序～
      */
 
-    public int[] bubbleSort (int[] arr) {
+    /**
+     * 冒泡排序
+     * @param arr
+     * @return
+     */
+    public static int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    //交换
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    /**
+     * 优化后的冒泡排序
+     * @param arr
+     * @return
+     */
+    public static int[] bubbleSort2(int[] arr) {
+        //外层循环是排序的趟数
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            //每比较一趟就重新初始化为0
+            int isChange = 0;
+
+            //内层循环是当前趟数需要比较的次数
+            for (int j = 0; j < arr.length - i - 1; j++) {
+
+                //前一位与后一位与前一位比较，如果前一位比后一位要大，那么交换
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
 
 
-        return null;
+                    //如果进到这里面了，说明发生置换了
+                    isChange = 1;
+
+                }
+            }
+            //如果比较完一趟没有发生置换，那么说明已经排好序了，不需要再执行下去了
+            if (isChange == 0) {
+                break;
+            }
+
+        }
+        return arr;
     }
 
     public static void main(String[] args) {
-
+        int[] arr = {1, 2, 5, 3, 7, 6};
+        int[] res = BubbleSort.bubbleSort(arr);
+        for (int i : res) {
+            System.out.println(i);
+        }
+        System.out.println("======================");
+        int[] res2 = BubbleSort.bubbleSort2(arr);
+        for (int i : res) {
+            System.out.println(i);
+        }
     }
 }
---
+++ DataStructure&Algorithms/Algorithms/src/main/java/SortingAlgorithm/QuickSort.java
@@ -0,0 +1,70 @@
+package SortingAlgorithm;
+
+import java.security.PrivateKey;
+
+/**
+ * 快速排序
+ * 时间复杂度O（nlogn）
+ *
+ * @author zhiyuanliu
+ * @date 2020/6/24 17:27
+ */
+public class QuickSort {
+
+    /**
+     * 快排
+     *
+     * @param arr 要进行排序的数组
+     * @param L   指向数组最左侧的元素
+     * @param R   指向数组最右侧的元素
+     * @return
+     */
+    public static void quickSort(int[] arr, int L, int R) {
+        // 使i等于最左侧数据的索引
+        int i = L;
+        // 使j等于最右侧数据的索引
+        int j = R;
+
+        // 找到中间值作为支点
+        int pivot = arr[(L + R) / 2];
+
+        // 当i<=j的情况下，不断的判断是否满足两边数据交换的条件
+        while (i <= j) {
+
+            // 寻找左侧比支点大的数据的索引
+            while (pivot > arr[i]) {
+                i++;
+            }
+            // 寻找右侧比支点大的数据的索引
+            while (pivot < arr[j]) {
+                j--;
+            }
+
+            // 若i<=j说明两端都已经找到需要交换的数据
+            if (i <= j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++; j--;
+            }
+        }
+
+        //“左边”再做排序，直到左边剩下一个数(递归出口)
+        if (L < j) {
+            quickSort(arr, L, j);
+        }
+
+        //“右边”再做排序，直到右边剩下一个数(递归出口)
+        if (i < R) {
+            quickSort(arr, i, R);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 5, 3, 7, 6};
+        QuickSort.quickSort(arr, 0, arr.length - 1);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
