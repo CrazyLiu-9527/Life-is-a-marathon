6da9dab56aa2c32e31f7af43b48a992ad0e96b73
zhiyuanliu <zhiyuanliu@ctrip.com>
2020-06-24 15:55:51 +0800

    Add files

---
+++ DataStructure&Algorithms/Algorithms/src/main/java/SortingAlgorithm/BubbleSort.java
@@ -0,0 +1,32 @@
+package SortingAlgorithm;
+
+/**
+ * 冒泡排序算法
+ * @author zhiyuanliu
+ * @date 2020/6/24 14:40
+ */
+public class BubbleSort {
+
+    /*
+     * 思路：
+     * 俩俩交换，大的放在后面，第一次排序后最大值已在数组末尾。
+     * 因为俩俩交换，需要n-1趟排序，比如10个数，需要9趟排序
+     *
+     * 代码实现要点：
+     * 两个for循环，外层循环控制排序的趟数，内层循环控制比较的次数
+     * 每趟过后，比较的次数都应该要减1
+     *
+     * 优化：
+     * 如果一趟排序后也没有交换位置，那么该数组已有序～
+     */
+
+    public int[] bubbleSort (int[] arr) {
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
