2a1883e0b2240bdc1b9b00ed139d8587bc644143
zhiyuanliu <zhiyuanliu@ctrip.com>
2020-07-16 17:27:36 +0800

    Add deadlock demo

---
+++ Concurrency/src/main/java/per/lzy/concurrencuylearning/deadlock/dynamicsequentialdeadlocks/TransferAccount.java
@@ -0,0 +1,27 @@
+package per.lzy.concurrencuylearning.deadlock.dynamicsequentialdeadlocks;
+
+/**
+ * @author zhiyuanliu
+ * @date 2020/7/16 12:11
+ */
+public class TransferAccount implements ITransfer {
+    /**
+     * 转账
+     *
+     * @param from   转出账户
+     * @param to     转入账户
+     * @param amount 转账金额
+     * @throws InterruptedException
+     */
+    @Override
+    public void transfer(UserAccount from, UserAccount to, int amount) throws InterruptedException {
+        synchronized (from) {
+            System.out.println(Thread.currentThread().getName() + " get " + from.getName());
+            synchronized (to) {
+                System.out.println(Thread.currentThread().getName() + " to " + to.getName());
+                from.flyMoney(amount);
+                to.addMoney(amount);
+            }
+        }
+    }
+}
